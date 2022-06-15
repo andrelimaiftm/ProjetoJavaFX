2e7c2757cd86bfb9c723d22dfd6bdc6364121fca
andrelimaiftm <andrelima@iftm.edu.br>
2022-06-15 11:30:23 -0300

    aula 14/06

--- src/main/java/br/edu/iftm/projetojavafx/Main.java
+++ src/main/java/br/edu/iftm/projetojavafx/Main.java
@@ -3,18 +3,31 @@ package br.edu.iftm.projetojavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private static Scene scene;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        ScrollPane scrollPane = fxmlLoader.load();
+
+        scrollPane.setFitToHeight(true);
+        scrollPane.setFitToWidth(true);
+
+        scene = new Scene(scrollPane);
         stage.setTitle("Projeto JavaFX");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static Scene getScene() {
+        return scene;
     }
 
     public static void main(String[] args) {
--- src/main/java/br/edu/iftm/projetojavafx/ViewController.java
+++ src/main/java/br/edu/iftm/projetojavafx/ViewController.java
@@ -1,14 +1,69 @@
 package br.edu.iftm.projetojavafx;
 
+import br.edu.iftm.projetojavafx.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ViewController implements Initializable {
 
+    @FXML
+    private MenuItem menuItemVendedor;
+
+    @FXML
+    private MenuItem menuItemDepartamento;
+
+    @FXML
+    private MenuItem menuItemSobre;
+
+    @FXML
+    public void onMenuItemVendedorAction(){
+        System.out.println("onMenuItemVendedorAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartamentoAction(){
+        System.out.println("onMenuItemDepartamentoAction");
+    }
+
+    @FXML
+    public void onMenuItemSobreAction(){
+        loadView("sobre.fxml");
+        //System.out.println("onMenuItemSobreAction");
+    }
+
+
+
+
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
+    }
+
+    public void loadView(String nomeDaView){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeDaView));
+            VBox novaVBox =  loader.load();
+            Scene scenePrincipal = Main.getScene();
+            VBox vboxPrincipal = (VBox) ((ScrollPane) scenePrincipal.getRoot()).getContent();
+            Node menuPrincipal = vboxPrincipal.getChildren().get(0);
+            vboxPrincipal.getChildren().clear();
+            vboxPrincipal.getChildren().add(menuPrincipal);
+            vboxPrincipal.getChildren().addAll(novaVBox.getChildren());
+
+        } catch (IOException e) {
+            Alerts.showAlert("IOException", "Erro ao carregar a view",
+                    e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
